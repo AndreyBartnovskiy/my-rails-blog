96dfa5cac5148d85627e7b3d326aaa28b1fb015e
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-03-08 15:13:20 +0300

    add devise gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+  #колбэк указывает данный метод, который вызывается до действия любого девайс контроллера
+  #с помощью этого метода можно добавлять различную реализацию работы иных параметров
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username])
+  end
 end
