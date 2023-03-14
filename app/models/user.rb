d4f9de69af7af50ba69566da6cf165438d8da2b2
Andrey Bartnovskiy <124582630+AndreyBartnovskiy@users.noreply.github.com>
2023-03-14 12:04:11 +0300

    Update app/models/user.rb
    Co-authored-by: Rasim Khusaenov <rasim.khusaenov@stackers.team>

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :first_name, :last_name, presence: true
 end
