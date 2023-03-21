123224c0291872ce5fa3f433bdecb6a6f95686f2
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-03-21 13:16:16 +0300

    change schema

--- db/migrate/20230315073052_add_draft_to_articles.rb
+++
@@ -1,5 +0,0 @@
-class AddDraftToArticles < ActiveRecord::Migration[6.1]
-  def change
-    add_column :articles, :draft, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_15_073052) do
+ActiveRecord::Schema.define(version: 2023_03_16_203950) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2023_03_15_073052) do
     t.text "body"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.boolean "draft", default: false
+    t.string "status", default: "published", null: false
   end
 
   create_table "users", force: :cascade do |t|
