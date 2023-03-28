3b1aab9d132ac5f4d012965dbd642091d1e2c59a
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-03-28 18:21:44 +0300

    refactor status updating

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   }
   root to:"articles#index"
 
-  resources :articles
+  resources :articles do 
+    resources :publications, only: %i[create destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
