6405b5f76badc17ba6b2dfa254cd96657cd49a58
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-04-05 17:08:00 +0300

    correct policy with relation scope

--- app/policies/article_policy.rb
+++ app/policies/article_policy.rb
@@ -1,6 +1,14 @@
 class ArticlePolicy < ApplicationPolicy
   authorize :user, allow_nil: true
 
+  relation_scope do |relation|
+    if user.nil?
+      relation.published
+    else
+      relation.where(user_id: user.id).or(relation.published)
+    end
+  end
+
   def index?
     true
   end
@@ -14,6 +22,24 @@ class ArticlePolicy < ApplicationPolicy
   end
 
   def show?
-    create?
+    true
+  end
+
+  def destroy?
+    owner?
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    owner?
+  end
+
+  private
+
+  def owner?
+    user.present? && record.user == user
   end
 end
