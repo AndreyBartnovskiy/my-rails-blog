147e67e9af9d5483832748b4942241329356c46e
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-04-05 15:19:08 +0300

    articles db table changed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_04_04_114641) do
+ActiveRecord::Schema.define(version: 2023_04_04_160056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 2023_04_04_114641) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "status", default: "published", null: false
-    t.bigint "user_id"
+    t.integer "user_id", null: false
     t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
