9d045d99c2e04b82e965931cf6a67d3d51aedc3e
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-04-06 13:07:05 +0300

    fix with rubocop

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   has_many :articles, dependent: :destroy
-  
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
