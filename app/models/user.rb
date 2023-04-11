373078fdfdc44f3304c05961ddf55a3144868771
AndreyBartnovskiy <andrey.bartnovskiy@stackers.team>
2023-04-11 13:14:23 +0300

    add admin role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+  include Enumerize
+
+  STATUSES = %i[user admin].freeze
+
   has_many :articles, dependent: :destroy
+
+  enumerize :status, in: STATUSES, scope: :shallow, predicate: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :first_name, :last_name, presence: true
+  validates :first_name, :last_name, :status, presence: true
 end
